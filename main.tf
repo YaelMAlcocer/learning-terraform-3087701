38c1aa436bcb4291b021e99367d24b194fdb7946
YaelMAlcocer <137358526+YaelMAlcocer@users.noreply.github.com>
2023-06-22 10:44:39 -0600

    From branch 04_02

--- main.tf
+++ main.tf
@@ -20,12 +20,10 @@ module "blog_vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
-  enable_vpn_gateway = true
 
   tags = {
     Terraform = "true"
@@ -33,27 +31,25 @@ module "blog_vpc" {
   }
 }
 
-module "autoscaling" {
+
+module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "6.10.0"
-  # insert the 1 required variable here
+  version = "6.5.2"
 
-  name      = "blog"
-  min_size  = 1
-  max_size  = 2
+  name = "blog"
 
+  min_size            = 1
+  max_size            = 2
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns   = module.blog_alb.target_group_arns
-
-  security_groups = [module.blog_sg.security_group_id]
-  
-  image_id      = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+  security_groups     = [module.blog_sg.security_group_id]
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
 
 module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
-  version = "~> 8.0"
+  version = "~> 6.0"
 
   name = "blog-alb"
 
@@ -74,14 +70,9 @@ module "blog_alb" {
 
   http_tcp_listeners = [
     {
-      port        = 80
-      protocol    = "HTTP"
-      action_type = "redirect"
-      redirect = {
-        port        = "443"
-        protocol    = "HTTPS"
-        status_code = "HTTP_301"
-      }
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
     }
   ]
 
@@ -92,14 +83,12 @@ module "blog_alb" {
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.0"
+  version = "4.13.0"
+
+  vpc_id  = module.blog_vpc.vpc_id
   name    = "blog"
-
-  vpc_id = module.blog_vpc.public_subnets[0]
-
-  ingress_rules       = ["http-80-tcp","https-443-tcp"]
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  egress_rules       = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]  
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
